a13c2b4e04f8aced0470e63ad7e40ac8e86dd47a
Michael Strupp <m.strupp@gmx.de>
2020-08-27 23:46:21 +0200

    initial commit

---
+++ src/main/java/org/chang/springboot/jms/JmsApplication.java
@@ -0,0 +1,14 @@
+package org.chang.springboot.jms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class JmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsApplication.class, args);
+	}
+
+}
